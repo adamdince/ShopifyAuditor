028ab812a7890933e09607297b8d849232059a69
adamdince <139258829+adamdince@users.noreply.github.com>
2025-07-11 12:03:36 -0500

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testDir: './tests',
+  timeout: 30000,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 0,
+    ignoreHTTPSErrors: true,
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...require('@playwright/test').devices['Desktop Chrome'] },
+    },
+  ],
+};
